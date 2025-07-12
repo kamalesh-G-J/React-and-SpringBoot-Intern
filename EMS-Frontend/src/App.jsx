0189ba76e24271fa7443bc48af69ffd0a71e0726
kamalesh-G-J <152084931+kamalesh-G-J@users.noreply.github.com>
2025-07-12 17:52:33 +0530

    React-Files

---
+++ EMS-Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import Login from './components/Login'
+
+function App() {
+  return(
+    <div>
+      <Login />
+    </div>  
+  )
+  
+}
+
+export default App
